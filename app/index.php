1a0bd98433ae139386ef21ee3bdf962d5869ab42
IsaacAndreu <isaacandreu@iesebre.com>
2022-11-07 10:05:34 +0100

    101 Acabat

--- app/index.php
+++ app/index.php
@@ -1,5 +1,32 @@
 <?php
 
-$name = 'Isaac';
+require 'app/helpers.php';
 
-$greeting = 'Hola $name';
+require 'app/Task.php';
+
+//$task = new Task(1,'comprar pa','a la panaderia', 0);
+//var_dump($task);
+
+$user= 'debian-sys-maint';
+$pass= 'XEMclraXyK0IqZkr';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user, $pass);
+} catch (\Exception $e) {
+    echo 'Error de connexió a la base de dades';
+}
+
+$statement = $dbh->prepare('SELECT * FROM tasks;');
+
+$statement->execute();
+
+
+// QUERY STRING
+//var_dump ($_GET['name']);
+//$name = 'Isaac';
+
+// API
+
+$greeting = greet();
+
+//$greeting = 'Hola ' . $_GET['name'] . $_GET['name'] '!';
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,26 @@
     </style>
 </head>
 <body>
-<h1><?=$greeting;?></h1>
+<header>
+    <h1><?=$greeting;?></h1>
+    <table>
+        <tr>
+           <th>Id</th>
+           <th>Title</th>
+           <th>Description</th>
+           <th>Completed</th>
+        </tr>
+        <?php foreach ($tasks as $task): ?>
+        <tr>
+            <td><?=$task->id;?></td>
+            <td><?=$task->title;?></td>
+            <td><?=$task->description;?></td>
+            <td><?=$task->completed;?></td>
+        </tr>
+        <?php endforeach; ?>
+
+    </table>
+</header>
 </body>
 </html>
 
