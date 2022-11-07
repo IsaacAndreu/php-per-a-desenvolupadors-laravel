165900679dfd3b0651e32f74c5b9d131279c814c
IsaacAndreu <isaacandreu@iesebre.com>
2022-11-07 15:58:00 +0100

    7/11/22

--- app/index.php
+++ app/index.php
@@ -4,14 +4,25 @@ require 'app/helpers.php';
 
 require 'app/Task.php';
 
-//$task = new Task(1,'comprar pa','a la panaderia', 0);
-//var_dump($task);
+require 'config.php';
 
-$user= 'debian-sys-maint';
-$pass= 'XEMclraXyK0IqZkr';
+//$user= 'debian-sys-maint';
+//$pass= 'XEMclraXyK0IqZkr';
+//$dsn= 'mysql:host=localhost;dbname=phplaraveldevs';
+
+
+
+$user= $config['database']['user'];
+$pass= $config['database']['password'];
+$type= $config['database']['databasetype'];
+$host= $config['database']['host'];
+$name= $config['database']['databasename'];
+$dsn= "$type:host=$host;dbname=$name";
+
+
 
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=phplaraveldevs', $user, $pass);
+    $dbh = new PDO($dsn, $user, $pass);
 } catch (\Exception $e) {
     echo 'Error de connexió a la base de dades';
 }
@@ -20,13 +31,6 @@ $statement = $dbh->prepare('SELECT * FROM tasks;');
 
 $statement->execute();
 
-
-// QUERY STRING
-//var_dump ($_GET['name']);
-//$name = 'Isaac';
-
-// API
-
 $greeting = greet();
 
-//$greeting = 'Hola ' . $_GET['name'] . $_GET['name'] '!';
+
