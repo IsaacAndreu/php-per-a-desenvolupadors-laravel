51e296c101bf430942d87640fb84b64b90f81f90
IsaacAndreu <isaacandreu@iesebre.com>
2023-01-04 18:16:21 +0100

    Versió 0.7

--- framework/bootstrap.php
+++ framework/bootstrap.php
@@ -2,9 +2,13 @@
 
 //use Framework\App;
 
+use Dotenv\Dotenv;
 use Framework\App;
 use frameWork\Database\Connection;
 use frameWork\Database\Database;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+$dotenv->load();
 
 App::bind('config', require 'config.php');
 
